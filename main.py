58a66e8a1a39ea865a5a99fefc1e21944ff66e85
Mustafa Berat <32976555+mustafaberat@users.noreply.github.com>
2021-03-26 23:13:39 +0300

    Update main.py
    mha dosyasi okumasi baslandi, mha dosyasi nii formatina donusturuldu. Nii formatindan png formati cevirmede hata ile karsilasildi

--- main.py
+++ main.py
@@ -5,43 +5,41 @@ import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
 import cv2
 from skimage.io import imread_collection
-
+import SimpleITK as sitk
 import os
-
-DATADIR ="brain_tumor_dataset/"
-CATEGORIES = ["yes","no"]
-
-# TODO: RESIM DONDURME OLMALI
-# TODO: PIKSEL BOZMA ISLEMLERI OLMALI
-
-#TODO: sadece yeslerle egitilebilir test_train yapinca
-for category in CATEGORIES:
-    path = os.path.join(DATADIR, category)
-    for img in os.listdir(path):
-        img_array = cv2.imread(os.path.join(path,img), cv2.IMREAD_GRAYSCALE)
-        plt.imshow(img_array, cmap="gray")
-        plt.show()
-        break
-    break
-
-# PIKSEL BOZMA
-# training_data = []
-# IMG_SIZE = 50
-# new_array = cv2.resize(img_array, (IMG_SIZE, IMG_SIZE))
-# plt.imshow(new_array, cmap = 'gray')
-# plt.show()
+import skimage.io as io
+import nibabel as nib
+import os
+import numpy as np
+import nibabel as nib
+import imageio
+import matplotlib
+from PIL import Image
+from nibabel.viewers import OrthoSlicer3D
 
 
+path = 'C:/Users/musta/.spyder-py3/bitirme/DataSet_Train/HGG/brats_2013_pat0001_1/VSD.Brain.XX.O.MR_T2.54515.mha' 
+outpath  = 'C:/Users/musta/.spyder-py3/bitirme/DataSet_Train/HGG/brats_2013_pat0001_1/asd.nii'
 
-def create_training_data():
-    for category in CATEGORIES:
-        path = os.path.join(DATADIR, category)
-        class_num = CATEGORIES.index(category)
-        for img in os.listdir(path):
-            try:
-                img_array = cv2.imread(os.path.join(path,img), cv2.IMREAD_GRAYSCALE)
-                new_array = cv2.resize(img_array, (IMG_SIZE, IMG_SIZE))
-                training_data.append([new_array, class_num])
-            except Exception as e:
-                pass
+img = io.imread(path,plugin='simpleitk')
+io.imsave(outpath, img,plugin='simpleitk')
 
+def read_niifile(niifilepath): #read niifile file
+    img = nib.load(niifilepath) #download niifile file (actually extract the file)
+    img_fdata = img.get_fdata() #Get niifile data
+    return img_fdata
+ 
+def save_fig(niifilepath,savepath): #Save as picture
+    fdata = read_niifile(niifilepath) #Call the above function to get data
+    (x,y,z) = fdata.shape #Get data shape information: (length, width, dimension-number of slices, fourth dimension)
+    for k in range(z):
+        silce = fdata[:,:,k] #Three positions represent three slices with different angles
+    imageio.imwrite(os.path.join(savepath,'{}.png'.format(k)),silce)
+                                                                                 #Save the slice information as png format
+ 
+if __name__=='__main__':
+    niifilepath='C:/Users/musta/.spyder-py3/bitirme/DataSet_Train/HGG/brats_2013_pat0001_1/asd.nii'
+    savepath='C:/Users/musta/.spyder-py3/bitirme/DataSet_Train/HGG/brats_2013_pat0001_1/AD'
+    if not os.path.exists(savepath):
+        os.makedirs(savepath)
+    save_fig(niifilepath,savepath)
