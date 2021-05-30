0d3ecf89398efc4703ba55342b5118d697c0e9a4
Sümeyye Varlık <sum.varlik1998@hotmail.com>
2021-05-30 17:23:34 +0300

    train.py added

---
+++ train.py
@@ -0,0 +1,158 @@
+import numpy as np
+import pandas as pd
+from model import unet_model
+import matplotlib.pyplot as plt
+import os
+import skimage.io as io
+import skimage.transform as trans
+import random as r
+import logging
+from keras.models import Sequential,load_model,Model,model_from_json
+from keras.layers import Dense, Dropout, Activation, Flatten
+from keras.layers import Convolution2D,concatenate, Conv2D, MaxPooling2D, Conv2DTranspose, UpSampling2D
+from keras.layers import Input, BatchNormalization
+from keras.regularizers import l2
+from keras.callbacks import ModelCheckpoint
+from keras.optimizers import Adam
+from keras import backend as K
+from keras.utils import plot_model
+
+
+img_size = 120
+
+# öğrenme oranı
+learning_rate = 1e-4
+.
+K.set_image_data_format('channels_first')
+
+# smooth degree of the images
+smooth = 1
+
+def dice_coef(y_true, y_pred):
+    y_true_f = K.flatten(y_true)
+    y_pred_f = K.flatten(y_pred)
+    intersection = K.sum(y_true_f * y_pred_f)
+    return (2. * intersection + smooth) / (K.sum(y_true_f) + K.sum(y_pred_f) + smooth)
+
+
+def dice_coef_loss(y_true, y_pred):
+    return 1-dice_coef(y_true, y_pred)
+
+def unet_model():
+    inputs = Input((1, img_size, img_size))
+    conv1 = Conv2D(64, (3, 3), activation='relu', padding='same') (inputs)
+    batch1 = BatchNormalization(axis=1)(conv1)
+    conv1 = Conv2D(64, (3, 3), activation='relu', padding='same') (batch1)
+    batch1 = BatchNormalization(axis=1)(conv1)
+    pool1 = MaxPooling2D((2, 2)) (batch1)
+
+    conv2 = Conv2D(128, (3, 3), activation='relu', padding='same') (pool1)
+    batch2 = BatchNormalization(axis=1)(conv2)
+    conv2 = Conv2D(128, (3, 3), activation='relu', padding='same') (batch2)
+    batch2 = BatchNormalization(axis=1)(conv2)
+    pool2 = MaxPooling2D((2, 2)) (batch2)
+
+    conv3 = Conv2D(256, (3, 3), activation='relu', padding='same') (pool2)
+    batch3 = BatchNormalization(axis=1)(conv3)
+    conv3 = Conv2D(256, (3, 3), activation='relu', padding='same') (batch3)
+    batch3 = BatchNormalization(axis=1)(conv3)
+    pool3 = MaxPooling2D((2, 2)) (batch3)
+
+
+    conv5 = Conv2D(512, (3, 3), activation='relu', padding='same') (pool3)
+    batch5 = BatchNormalization(axis=1)(conv5)
+    conv5 = Conv2D(512, (3, 3), activation='relu', padding='same') (batch5)
+    batch5 = BatchNormalization(axis=1)(conv5)
+
+
+    up7 = Conv2DTranspose(256, (2, 2), strides=(2, 2), padding='same') (batch5)
+    up7 = concatenate([up7, conv3], axis=1)
+    conv7 = Conv2D(256, (3, 3), activation='relu', padding='same') (up7)
+    batch7 = BatchNormalization(axis=1)(conv7)
+    conv7 = Conv2D(256, (3, 3), activation='relu', padding='same') (batch7)
+    batch7 = BatchNormalization(axis=1)(conv7)
+
+    up8 = Conv2DTranspose(128, (2, 2), strides=(2, 2), padding='same') (batch7)
+    up8 = concatenate([up8, conv2], axis=1)
+    conv8 = Conv2D(128, (3, 3), activation='relu', padding='same') (up8)
+    batch8 = BatchNormalization(axis=1)(conv8)
+    conv8 = Conv2D(128, (3, 3), activation='relu', padding='same') (batch8)
+    batch8 = BatchNormalization(axis=1)(conv8)
+
+    up9 = Conv2DTranspose(64, (2, 2), strides=(2, 2), padding='same') (batch8)
+    up9 = concatenate([up9, conv1], axis=1)
+    conv9 = Conv2D(64, (3, 3), activation='relu', padding='same') (up9)
+    batch9 = BatchNormalization(axis=1)(conv9)
+    conv9 = Conv2D(64, (3, 3), activation='relu', padding='same') (batch9)
+    batch9 = BatchNormalization(axis=1)(conv9)
+
+    conv10 = Conv2D(1, (1, 1), activation='sigmoid')(batch9)
+
+    model = Model(inputs=[inputs], outputs=[conv10])
+
+    model.compile(loss=dice_coef_loss, metrics=[dice_coef], optimizer=Adam(lr=learning_rate))
+    #model.compile(loss='binary_crossentropy',metrics=['acc'], optimizer=Adam(lr=learning_rate))
+
+    return model
+
+
+
+# plot_model(unet_model(), to_file='model.png')
+
+num_epoch = 20
+
+#array e dönüştürülmüş dataset yüklenir
+print("Loading dataset.")
+train_X = np.load('D:/Kullanıcılar/.spyder-py3/bitirme/x_{}.npy'.format(img_size))
+train_Y = np.load('D:/Kullanıcılar/.spyder-py3/bitirme/y_{}.npy'.format(img_size))
+
+
+#eğitim
+num = 31100
+
+model = unet_model()
+history = model.fit(train_X, train_Y, batch_size=16, validation_split=0.2 ,epochs= num_epoch, verbose=1, shuffle=True)
+
+
+#%%
+# model ağırlıkları ile kaydedilir
+model.save_weights('weights_{}_{}.h5'.format(img_size,num_epoch))
+#model.load_weights('weights.h5')
+
+
+#%%
+# For binary metrics
+plt.plot(history.history['acc'])
+plt.plot(history.history['val_acc'])
+plt.title('Model accuracy')
+plt.ylabel('Accuracy')
+plt.xlabel('Epoch')
+plt.legend(['Train', 'Test'], loc='upper left')
+plt.show()
+# Plot training & validation loss values
+plt.plot(history.history['loss'])
+plt.plot(history.history['val_loss'])
+plt.title('Model loss')
+plt.ylabel('Loss')
+plt.xlabel('Epoch')
+plt.legend(['Train', 'Test'], loc='upper left')
+plt.show()
+
+# Plot training & validation accuracy values
+
+plt.plot(history.history['dice_coef'])
+plt.plot(history.history['val_dice_coef'])
+plt.title('model dice_coef')
+plt.ylabel('dice_coef')
+plt.xlabel('epoch')
+plt.legend(['train', 'validation'], loc='upper left')
+plt.show()
+
+# summarize history for loss
+plt.plot(history.history['loss'])
+plt.plot(history.history['val_loss'])
+plt.title('model loss')
+plt.ylabel('loss')
+plt.xlabel('epoch')
+plt.legend(['train', 'test'], loc='upper left')
+plt.show()
